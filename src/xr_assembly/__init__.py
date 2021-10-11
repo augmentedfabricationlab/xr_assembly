cfd05f45ef23c480850a84485416026e29e9f37d
lidiatanasova <39670477+lidiatanasova@users.noreply.github.com>
2021-10-11 17:37:15 +0200

    Update __init__.py

--- src/xr_assembly/__init__.py
+++ src/xr_assembly/__init__.py
@@ -1,2 +1,25 @@
-from .assembly import XRAssembly
-from .element import XRElement
+"""
+
+Intro to project ...
+
+
+Setup
+=====
+
+In order to use this library, ...
+
+
+Main concepts
+=============
+
+Describe typical classes found in project
+
+.. autoclass:: SampleClassName
+   :members:
+
+
+"""
+
+from .sample_module import SampleClassName
+
+__all__ = ['SampleClassName']
